85cfb747cb08b588a572a094deff298916e1f3aa
Numo <nanateinumonde@gmail.com>
2025-02-22 14:29:45 +0100

    fix: Updated docker-compose

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-set -e
-
-TEMPLATE="/etc/nginx/conf.d/default.conf.template"
-CONFIG="/etc/nginx/conf.d/default.conf"
-
-# Check if the template exists
-if [ -f "$TEMPLATE" ]; then
-    envsubst '$DOMAIN_NAME' < "$TEMPLATE" > "$CONFIG"
-else
-    echo "Error: $TEMPLATE not found. Ensure it's correctly mapped in docker-compose."
-    exit 1
-fi
-
+# Substitute environment variables in the Nginx config file
+envsubst '$DOMAIN_NAME' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 # Start Nginx
 nginx -g "daemon off;"
